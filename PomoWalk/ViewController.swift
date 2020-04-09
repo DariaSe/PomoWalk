7597748538c16ce2c78ffc5f35dfaa700f12b6de
Daria Selezneva <daria.samosledova@icloud.com>
2020-04-09 09:57:38 +0300

    Initial Commit

---
+++ PomoWalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PomoWalk
+//
+//  Created by Дарья Селезнёва on 09.04.2020.
+//  Copyright © 2020 dariaS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
